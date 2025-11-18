71cba10449ec321a8944046845c9e05b132a0ed3
Mohammed2372 <mohammedmostafa2372002@gmail.com>
2025-11-18 20:13:59 +0200

    dynamically change the serializer based on how the product list viewed

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,9 +1,9 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions, mixins
 from django_filters.rest_framework import DjangoFilterBackend
 
 
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, ProductDetailSerializer, Cart, CartItem
 from .permissions import IsAdminOrReadOnly
 from .filters import ProductFilter
 
@@ -12,7 +12,13 @@ from .filters import ProductFilter
 ## Product with *ModelViewSet*
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+
+    # Dynamically switches serializers based on the request type (list or detail)
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProductSerializer
+        return ProductDetailSerializer
+
     permission_classes = [IsAdminOrReadOnly]
     ordering_fields = ["price", "created_at"]
     filter_backends = [
@@ -22,4 +28,3 @@ class ProductViewSet(viewsets.ModelViewSet):
     ]
     filterset_class = ProductFilter
     search_fields = ["name", "description"]
-
