ac117d1923f12d59ba00a301492b98296f66d7ce
Mohammed2372 <mohammedmostafa2372002@gmail.com>
2025-11-19 13:04:27 +0200

    update fields of product and product detail serializers

--- ecommerce/serializers.py
+++ ecommerce/serializers.py
@@ -12,12 +12,10 @@ class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            "id",
             "name",
             "category",
             "price",
             "in_stock",
-            "created_at",
         ]
 
 
@@ -28,7 +26,15 @@ class ProductDetailSerializer(ModelSerializer):
 
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = fields = [
+            "id",
+            "name",
+            "description",
+            "category",
+            "price",
+            "in_stock",
+            "created_at",
+        ]
 
 
 class CategorySerializer(ModelSerializer):
@@ -44,17 +50,18 @@ class CartItemSerializer(ModelSerializer):
         queryset=Product.objects.all(), source="product", write_only=True
     )
     subtotal = serializers.ReadOnlyField()
+
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'product_id', 'quantity', 'subtotal']
-        extra_kwargs = {'cart': {'read_only': True}} 
-        
-        
+        fields = ["id", "product", "product_id", "quantity", "subtotal"]
+        extra_kwargs = {"cart": {"read_only": True}}
+
+
 class CartSerializer(ModelSerializer):
     items = CartItemSerializer(many=True, read_only=True)
     total_price = serializers.ReadOnlyField()
     user = serializers.StringRelatedField(read_only=True)
-    
+
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'status', 'items', 'total_price', 'created_at']
+        fields = ["id", "user", "status", "items", "total_price", "created_at"]
